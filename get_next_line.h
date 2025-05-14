cbdd1253628e409e64d5277b24d86d89ffdba96b
Julien Romain Peter Romann <jromann@c3r7p12.42vienna.com>
2025-05-14 15:47:19 +0200

    k

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jromann <jromann@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 16:50:41 by jromann           #+#    #+#             */
+/*   Updated: 2025/05/13 18:24:06 by jromann          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2, int specifier);
+char	*ft_read(char *arr, int fd);
+char	*ft_trim(char *arr, int index);
+int		ft_next_line_check(char *str, int specifier);
+
+#endif
---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jromann <jromann@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 16:50:41 by jromann           #+#    #+#             */
+/*   Updated: 2025/05/14 11:01:33 by jromann          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2, int specifier);
+char	*ft_read(char *arr, int fd);
+char	*ft_trim(char *arr, int index);
+int		ft_next_line_check(char *str, int specifier);
+
+#endif
